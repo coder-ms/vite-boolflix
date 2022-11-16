60aed1c80bb13224972643a556cdb9ff858bedac
coder-ms <marco.sbrascia@gmail.com>
2022-11-17 00:59:02 +0200

    add MovieComponent

--- src/components/MovieComponent.vue
+++ src/components/MovieComponent.vue
@@ -1,120 +1,71 @@
 <template>
-  <div v-bind:class="flipped ? 'flip-container flipped' : 'flip-container'">
-    <div class="flipper">
-      <div class="front">
-        <slot name="front"></slot>
-        <v-icon class="frontFlipBtn" v-on:click="flipped = true">
-          redo
-        </v-icon>
+  <div class="cardMovie">
+    <div class="box">
+
+      <div class="imgMovie">
+        <img :src="store.pathImgURL + movieTag.poster_path" :alt="movieTag.name">
       </div>
-      <div class="back">
-        <slot name="back"></slot>
-        <v-icon class="backFlipBtn" v-on:click="flipped = false">
-          redo
-        </v-icon>
+
+      <div class="movieInfo">
+        <h6>{{ movieTag.title }}</h6>
+        <p class="font-weight-bold">{{ movieTag.original_title }}</p>
+        <!--<img class="flagx" :src="store.getFlagImg(movieTag.original_language)" alt="">-->
+        <p>{{ movieTag.original_language }}</p>
+        <p>{{ movieTag.vote_average }} / 10</p>
       </div>
+
     </div>
   </div>
 </template>
 
 <script>
+import { store } from '../store';
 
 export default {
-  name: 'FlipCard',
-  data: function () {
+  data() {
     return {
-      flipped: false
+      store
+    }
+  },
+
+  props: {
+    movieTag: Object
+  }
+}
+</script>
+
+
+<style lang="scss" scoped>
+.cardMovie {
+  .box {
+    width: 100%;
+
+    .imgMovie {
+      margin-bottom: 10px;
+
+      img {
+        width: 100%;
+      }
+
+      .movieInfo {
+        padding: 5px;
+        margin-top: 10px;
+
+        h6 {
+          text-transform: capitalize;
+          font-weight: 700;
+        }
+
+        p {
+          font-size: 14px;
+          color: rgb(221, 220, 220);
+        }
+
+        .flagx {
+          width: 10%;
+        }
+      }
     }
   }
 }
-
-</script>
-
-<style lang="scss" scoped>
-i.frontFlipBtn,
-i.backFlipBtn {
-  position: absolute;
-  right: 20px;
-  top: 20px;
-  color: #FFFFFF;
-}
-
-i.backFlipBtn {
-  -webkit-transform: rotateY(-180deg);
-  -moz-transform: rotateY(-180deg);
-  -o-transform: rotateY(-180deg);
-  -ms-transform: rotateY(-180deg);
-  transform: rotateY(-180deg);
-}
-
-.flip-container {
-  -webkit-perspective: 1000;
-  -moz-perspective: 1000;
-  -o-perspective: 1000;
-  perspective: 1000;
-}
-
-.flip-container {
-  min-height: 120px;
-}
-
-.flipper {
-  position: relative;
-}
-
-.front,
-.back {
-  -webkit-backface-visibility: hidden;
-  -moz-backface-visibility: hidden;
-  -o-backface-visibility: hidden;
-  backface-visibility: hidden;
-  -webkit-transition: 0.6s;
-  -webkit-transform-style: preserve-3d;
-  -moz-transition: 0.6s;
-  -moz-transform-style: preserve-3d;
-  -o-transition: 0.6s;
-  -o-transform-style: preserve-3d;
-  -ms-transition: 0.6s;
-  -ms-transform-style: preserve-3d;
-  transition: 0.6s;
-  transform-style: preserve-3d;
-  top: 0;
-  left: 0;
-  width: 100%;
-}
-
-.back {
-  -webkit-transform: rotateY(-180deg);
-  -moz-transform: rotateY(-180deg);
-  -o-transform: rotateY(-180deg);
-  -ms-transform: rotateY(-180deg);
-  transform: rotateY(-180deg);
-  position: absolute;
-}
-
-.flip-container.flipped .back {
-  -webkit-transform: rotateY(0deg);
-  -moz-transform: rotateY(0deg);
-  -o-transform: rotateY(0deg);
-  -ms-transform: rotateY(0deg);
-  transform: rotateY(0deg);
-}
-
-.flip-container.flipped .front {
-  -webkit-transform: rotateY(180deg);
-  -moz-transform: rotateY(180deg);
-  -o-transform: rotateY(180deg);
-  -ms-transform: rotateY(180deg);
-  transform: rotateY(180deg);
-}
-
-.front {
-  z-index: 2;
-}
-
-
-
-
-
-/* Position the front and back side */
 </style>
