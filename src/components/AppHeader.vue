32682d2440dc3eec42348196dcf83fe736306d13
coder-ms <marco.sbrascia@gmail.com>
2022-11-18 13:00:49 +0200

    adjust navbar

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,68 +1,44 @@
 <template>
     <header>
-        <nav class="navbar navbar-expand-lg">
-            <div class="container-fluid">
-                <a class="navbar-brand" href="#">BOOLFLIX</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-                    aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                        <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="#">Home</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Link</a>
-                        </li>
-                        <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
-                                data-bs-toggle="dropdown" aria-expanded="false">
-                                Dropdown
-                            </a>
-                            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                <li><a class="dropdown-item" href="#">Action</a></li>
-                                <li><a class="dropdown-item" href="#">Another action</a></li>
-                                <li>
-                                    <hr class="dropdown-divider">
-                                </li>
-                                <li><a class="dropdown-item" href="#">Something else here</a></li>
-                            </ul>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-                        </li>
-                    </ul>
-                    <form class="row row-cols-lg-auto g-3 align-items-center" @submit.prevent="searchMovies">
-                        <!--SELECT MOVIE OR TV-->
-                        <div class="col-12">
-                            <label class="visually-hidden" for="searchOptions">Select series</label>
-                            <select class="form-select" id="searchOptions" v-model="store.searchSeries">
-                                <option selected value="">Choose...</option>
-                                <option :value="status" v-for="(status, index) in searchOptions" :key="index">{{ status
-                                }}</option>
-                            </select>
-                        </div>
+        <nav class="navBar col-12 col-lg-12 col-md-12 col-sm-12">
+            <div class="boolflixMenu col-8 col-lg-8 col-md-8 col-sm-8">
+                <a class="navbarBrand" href="#">BOOLFLIX</a>
+                <ul class="col-6 col-lg-6 col-md-6 col-sm-6">
+                    <li><a href="#home">Home</a></li>
+                    <li><a href="#news">News</a></li>
+                    <li><a href="#contact">Contact</a></li>
+                    <li><a href="#about">About</a></li>
+                </ul>
+            </div>
+            <div class="navbarCollapse col-4 col-lg-4 col-md-4 col-sm-4" id="navbarSupportedContent">
+                <form class="row row-cols-lg-auto g-3 align-items-center" @submit.prevent="searchMovies">
+                    <!--SELECT MOVIE OR TV-->
+                    <div class="col-12 col-lg-5 col-md-4 col-sm-4">
+                        <label class="visually-hidden" for="searchOptions">Select series</label>
+                        <select class="form-select" id="searchOptions" v-model="store.searchSeries">
+                            <option selected value="">Choose...</option>
+                            <option :value="status" v-for="(status, index) in searchOptions" :key="index">{{ status
+                            }}</option>
+                        </select>
+                    </div>
 
-                        <!-- TITLE FILTER -->
-                        <div class="col-12">
-                            <label class="visually-hidden" for="charactername">Search name</label>
-                            <input type="text" class="form-control" id="charactername" placeholder="Search name"
-                                v-model.trim="store.searchTitleVal">
-                        </div>
+                    <!-- TITLE FILTER -->
+                    <div class="col-12 col-lg-5 col-md-4 col-sm-4">
+                        <label class="visually-hidden" for="charactername">Search name</label>
+                        <input type="text" class="form-control" id="charactername" placeholder="Search name"
+                            v-model.trim="store.searchTitleVal">
+                    </div>
 
-                        <!--BUTTON SUBMIT-->
-                        <div class="col-12">
-                            <button type="submit" class="btn btn-primary">Search</button>
-                        </div>
+                    <!--BUTTON SUBMIT-->
+                    <div class="col-12 col-lg-1 col-md-2 col-sm-2">
+                        <button type="submit" class="searchButton">&#x1f50e;</button>
+                    </div>
 
-                        <!--BUTTON RESET-->
-                        <div class="col-12">
-                            <button type="reset" class="btn btn-primary" @click="resetSearch">Reset</button>
-                        </div>
-                    </form>
-                </div>
+                    <!--BUTTON RESET-->
+                    <div class="col-12 col-lg-1 col-md-2 col-sm-2">
+                        <button type="reset" class="resetButton" @click="resetSearch">&#8634;</button>
+                    </div>
+                </form>
             </div>
         </nav>
     </header>
@@ -107,14 +83,63 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.navbar {
+.navBar {
     background-color: grey;
+    display: flex;
+    height: 60px;
+    padding: 10px;
 
-    .container-fluid {
-        .navbar-brand {
-            padding: 0 10px;
+    .boolflixMenu {
+        display: flex;
+        align-items: center;
+
+        .navbarBrand {
             background-color: red;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            text-decoration: none;
             color: black;
+            font-weight: 600;
+            font-size: 20px;
+            height: 100%;
+        }
+
+        ul {
+            list-style-type: none;
+            margin: 0;
+            padding: 0;
+
+            li {
+                display: inline;
+                margin: 0 10px;
+
+                a {
+                    text-decoration: none;
+                    color: black;
+                }
+            }
+        }
+
+    }
+
+    .navbarCollapse {
+        form {
+            div {
+
+                .searchButton {
+                    border-radius: 40%;
+                    margin: 0 5px;
+                }
+            }
+
+            div {
+
+                .resetButton {
+                    border-radius: 50%;
+                    margin: 0 5px;
+                }
+            }
         }
     }
 }
